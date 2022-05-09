417dd959a6072fd480e445050737fbdecb9b6ff9
UcezZ <ucezz@mail.ru>
2022-05-09 14:55:08 +0300

    try add jsp

--- src/main/java/net/sytes/ucezz/mirea/java/carservice/security/SecurityConfig.java
+++ src/main/java/net/sytes/ucezz/mirea/java/carservice/security/SecurityConfig.java
@@ -1,29 +1,29 @@
 package net.sytes.ucezz.mirea.java.carservice.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+// import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+// import org.springframework.security.crypto.password.PasswordEncoder;
 
-import net.sytes.ucezz.mirea.java.carservice.service.CustomUserDetailsService;
+import net.sytes.ucezz.mirea.java.carservice.service.UserService;
 
 @EnableWebSecurity(debug = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
-    private CustomUserDetailsService userDetailsService;
+    private UserService userDetailsService;
 
     @Autowired
     private CustomAuthencationProvider customAuthencationProvider;
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
+    // @Bean
+    // public PasswordEncoder passwordEncoder() {
 
-        return NoOpPasswordEncoder.getInstance();
-    }
+    // return NoOpPasswordEncoder.getInstance();
+    // }
 
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {

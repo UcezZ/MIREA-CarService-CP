3415f1409a9d1fc59e5b637830722e83650fc291
UcezZ <ucezz@mail.ru>
2022-05-10 15:28:47 +0300

    css extension; transport entity extension

--- src/main/java/net/sytes/ucezz/mirea/java/carservice/controller/TransportController.java
+++ src/main/java/net/sytes/ucezz/mirea/java/carservice/controller/TransportController.java
@@ -1,12 +1,16 @@
 package net.sytes.ucezz.mirea.java.carservice.controller;
 
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import net.sytes.ucezz.mirea.java.carservice.entity.Transport;
 import net.sytes.ucezz.mirea.java.carservice.entity.User;
+import net.sytes.ucezz.mirea.java.carservice.repository.TransportRepository;
 import net.sytes.ucezz.mirea.java.carservice.repository.UserRepository;
 
 @Controller
@@ -14,11 +18,15 @@ public class TransportController {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    TransportRepository transportRepository;
+
     @RequestMapping("/transport")
     public String transport(Model model) {
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
         User user = userRepository.get(username);
-        model.addAttribute("user", user);
+        Set<Transport> allTransport = transportRepository.getAllByUserId(user.getId());
+        model.addAttribute("allTransport", allTransport);
         return "transport";
     }
 }

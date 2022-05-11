2e29e07f3c40a5d0c6b37b8d204e6f5d5d2ca344
UcezZ <ucezz@mail.ru>
2022-05-11 11:41:55 +0300

    registration fix sql

--- src/main/java/net/sytes/ucezz/mirea/java/carservice/repository/UserRepository.java
+++ src/main/java/net/sytes/ucezz/mirea/java/carservice/repository/UserRepository.java
@@ -19,7 +19,7 @@ public interface UserRepository extends CrudRepository<User, Long> {
     @Query(value = "select * from t_user", nativeQuery = true)
     List<User> getAll();
 
-    @Query(value = "insert into t_user (is_manager, last_name, first_name, middle_name, username, password_hash_string, birth_date) values ('false', ?1, ?2, ?3, ?4, ?5, ?6) union select ''", nativeQuery = true)
+    @Query(value = "insert into t_user (is_manager, last_name, first_name, middle_name, username, password_hash_string, birth_date) values ('false', ?1, ?2, ?3, ?4, ?5, ?6); select '';", nativeQuery = true)
     void register(String lastName, String firstName, String middleName, String username,
             String passwordHashString, String birthDate);
 }

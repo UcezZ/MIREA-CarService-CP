642b6324be2aebf2bd89ecfe44c256c681059dc1
UcezZ <ucezz@mail.ru>
2022-05-11 20:27:44 +0300

    removed unused imports

--- src/main/java/net/sytes/ucezz/mirea/java/carservice/controller/LoginController.java
+++ src/main/java/net/sytes/ucezz/mirea/java/carservice/controller/LoginController.java
@@ -1,19 +1,14 @@
 package net.sytes.ucezz.mirea.java.carservice.controller;
 
-import java.security.MessageDigest;
 import java.util.Set;
-
-import javax.xml.bind.DatatypeConverter;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.request.WebRequest;
 
-import net.sytes.ucezz.mirea.java.carservice.entity.User;
 import net.sytes.ucezz.mirea.java.carservice.repository.UserRepository;
 
 @Controller
